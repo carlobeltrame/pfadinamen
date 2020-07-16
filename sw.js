1a7cea992fed7893a6b06d5b24b7d340d2ff606a
Carlo Beltrame <carlo@beltra.me>
2020-07-16 10:16:27 +0200

    Small improvements to service worker

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const cacheName = 'Temporas';
+const cacheName = 'pfadinamen';
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      // Our application only has two files here index.html and manifest.json
-      // but you can add more such as style.css as your app grows
       return cache.addAll([
         './',
         './termynal.css',
@@ -20,9 +18,6 @@ self.addEventListener('install', e => {
   );
 });
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.open(cacheName)
